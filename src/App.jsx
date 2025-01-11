4fb7312c2b735f50a9297c6e4fc209480e94ce0b
Kevin Solar <kevinleitesolar@hotmail.com>
2025-01-11 17:35:16 -0300

    Adicionando novo produto e refatorando a lógica de fetch com um custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,110 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
 const url = "http://localhost:3000/produtos";
 
 function App() {
-	// const [listaProdutos] = useState(dataBase);
-	const [produtos, setProdutos] = useState([]);
-	const [nome, setNome] = useState([]);
-	const [preco, setPreco] = useState([]);
+   // const [listaProdutos] = useState(dataBase);
+   const [produtos, setProdutos] = useState([]);
+   const [nome, setNome] = useState([]);
+   const [preco, setPreco] = useState([]);
 
-	//1 - Resgatando dados
-	useEffect(() => {
-		async function fetchData() {
-			//primeiro vamos fazer com que espere receber as informações da url, que no caso está vinculado a variavel 'url'.
-			const res = await fetch(url);
+   const { data: itens, httpConfig, loading } = useFetch(url);
 
-			//após receber a confirmação de await da url(res) com as informações, vamos transformar isso em json interno.
-			const data = await res.json();
+   //1 - Resgatando dados
+   // useEffect(() => {
+   // 	async function fetchData() {
+   // 		//primeiro vamos fazer com que espere receber as informações da url, que no caso está vinculado a variavel 'url'.
+   // 		const res = await fetch(url);
 
-			//agora vamos salvar isso no nosso state de Produtos.
-			setProdutos(data);
-		}
+   // 		//após receber a confirmação de await da url(res) com as informações, vamos transformar isso em json interno.
+   // 		const data = await res.json();
 
-		fetchData();
-	}, []);
+   // 		//agora vamos salvar isso no nosso state de Produtos.
+   // 		setProdutos(data);
+   // 	}
 
-	//2 - Adição de produtos
-	const handleSubmit = async (submit) => {
-		submit.preventDefault();
+   // 	fetchData();
+   // }, []);
 
-		const produto = {
-			nome,
-			preco,
-		};
+   //2 - Adição de produtos
+   const handleSubmit = async (submit) => {
+      submit.preventDefault();
 
-		const res = await fetch(url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(produto),
-		});
+      const produto = {
+         nome,
+         preco,
+      };
 
-		//3 - Carregamento dinamico dos dados
-		/* Como não podemos adicionar diretamente o res, pois ele é um json,
-        vamos transformar ele em um objeto JavaScript, dessa forma:  */
-		const produtosAdicionados = await res.json();
+      // const res = await fetch(url, {
+      // 	method: "POST",
+      // 	headers: {
+      // 		"Content-Type": "application/json",
+      // 	},
+      // 	body: JSON.stringify(produto),
+      // });
 
-		/* Agora faço com que esses dados sejam acressentados ao meu setprodutos, para fazer o incremento de dados. */
-		setProdutos((prevProdutos) => [...prevProdutos, produtosAdicionados]);
-		  //Utilizamos o spred operator para "espalhar os dados anteriores" e logo após adicionar um novo, com o a variavel após a virgula
+      // //3 - Carregamento dinamico dos dados
+      // /* Como não podemos adicionar diretamente o res, pois ele é um json,
+      //   vamos transformar ele em um objeto JavaScript, dessa forma:  */
+      // const produtosAdicionados = await res.json();
+
+      // /* Agora faço com que esses dados sejam acressentados ao meu setprodutos, para fazer o incremento de dados. */
+      // setProdutos((prevProdutos) => [...prevProdutos, produtosAdicionados]);
+      //   //Utilizamos o spred operator para "espalhar os dados anteriores" e logo após adicionar um novo, com o a variavel após a virgula
+
+      {/* 5- Refatorando POST */ }
+      httpConfig(produto, "POST");
 
       //Após a adição, vamos zerar os inputs com:
       setNome("");
-      setPreco("");  
-	};
+      setPreco("");
+   };
 
-	return (
-		<main className="App">
-			<h1>Lista de produtos</h1>
+   return (
+      <main className="App">
+         <h1>Lista de produtos</h1>
 
-			<ul>
-				{produtos.map((produto) => (
-					<li key={produto.id}>
-						{produto.nome} - R$ {produto.preco}
-					</li>
-				))}
-			</ul>
+         {loading && <p>Carregando...</p>}
 
-			<div className="add-produto">
-				<form onSubmit={handleSubmit}>
-					<label>
-						Nome:
-						<input
-							type="text"
-							value={nome}
-							name="nome"
-							onChange={(e) => setNome(e.target.value)}
-						/>
-					</label>
-					<label>
-						Preço:
-						<input
-							type="number"
-							value={preco}
-							name="preco"
-							onChange={(e) => setPreco(e.target.value)}
-						/>
-					</label>
-					<input type="submit" value="Criar" />
-				</form>
-			</div>
-		</main>
-	);
+         {!loading && (
+            <ul>
+               {itens && itens.map((produto) => (
+                  <li key={produto.id}>
+                     {produto.nome} - R$ {produto.preco}
+                  </li>
+               ))}
+            </ul>
+         )}
+
+         <div className="add-produto">
+            <form onSubmit={handleSubmit}>
+               <label>
+                  Nome:
+                  <input
+                     type="text"
+                     value={nome}
+                     name="nome"
+                     onChange={(e) => setNome(e.target.value)}
+                  />
+               </label>
+               <label>
+                  Preço:
+                  <input
+                     type="number"
+                     value={preco}
+                     name="preco"
+                     onChange={(e) => setPreco(e.target.value)}
+                  />
+               </label>
+               <input type="submit" value="Criar" />
+            </form>
+         </div>
+      </main>
+   );
 }
 
 export default App;
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+
+
+//4 - custom hook
+export const useFetch = (url) => {
+   const [data, setData] = useState(null);
+
+   {/* 5 - refatorando o POST */ }
+   //config -> sera responsavel por fazer a confiugração do metodo, headers e body.
+   const [config, setConfig] = useState(null);
+
+   //method -> sera responsavel por fazer a configuração do metodo. ex: POST, GET, PUT, DELETE
+   const [method, setMethod] = useState(null);
+
+   //callFetch -> sera responsavel por ser mapeado, sempre que eu altera-lo, vou chamar o Fetch novamente para poder trazer os dados de novo.
+   const [callFetch, setCallFetch] = useState(false);
+
+   {/* 6 - Loading */}
+   const [loading, setLoading] = useState(false);
+
+   //Fazendo a configuração do metodo POST automaticamente, para não precisar ficar passando toda vez que for fazer um POST.
+   const httpConfig = (data, method) => {
+      if (method === "POST") {
+         setConfig({
+            method,
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+         });
+
+         setMethod(method);
+      }
+   }
+
+   useEffect(() => {
+      const fetchData = async () => {
+         //6 - Loading
+         setLoading(true);
+
+         const res = await fetch(url);
+         const json = await res.json();
+         setData(json);
+
+         setLoading(false);
+      }
+
+      fetchData();
+   }, [url, callFetch]);
+
+   {/* 5 - refatorando o POST */ }
+   useEffect(() => {
+      const httpRequest = async () => {
+         if (method === "POST") {
+            let fetchOptions = [url, config];
+
+            const res = await fetch(...fetchOptions);
+
+            const json = await res.json();
+
+            setCallFetch(json)
+         }
+      }
+
+      httpRequest();
+   }, [config, method, url]);
+
+   return { data, httpConfig, loading };
+}
